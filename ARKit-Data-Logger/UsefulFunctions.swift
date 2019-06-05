4ffb3d242cac6c6834cff8edcf75305c84ebdda2
pjinkim-Mac-PC <pjinkim1215@gmail.com>
2019-06-05 10:56:39 +0900

    add text file input/output interface.

---
+++ ARKit-Data-Logger/UsefulFunctions.swift
@@ -0,0 +1,36 @@
+//
+//  UsefulFunctions.swift
+//  ARKit-Data-Logger
+//
+//  Created by kimpyojin on 05/06/2019.
+//  Copyright © 2019 Pyojin Kim. All rights reserved.
+//
+
+import Foundation
+
+
+func interfaceIntTime(second: Int64) -> String {
+    var input = second
+    let hours: Int64 = input / 3600
+    input = input % 3600
+    let mins: Int64 = input / 60
+    let secs: Int64 = input % 60
+    
+    guard second >= 0 else {
+        fatalError("Second can not be negative: \(second)");
+    }
+    
+    return String(format: "%02d:%02d:%02d", hours, mins, secs)
+}
+
+func timeToString() -> String {
+    let date = Date()
+    let calendar = Calendar.current
+    let year = calendar.component(.year, from: date)
+    let month = calendar.component(.month, from: date)
+    let day = calendar.component(.day, from: date)
+    let hour = calendar.component(.hour, from: date)
+    let minute = calendar.component(.minute, from: date)
+    let sec = calendar.component(.second, from: date)
+    return String(format:"%04d%02d%02d_%02d%02d%02dt", year, month, day, hour, minute, sec)
+}
--- ARKit-Data-Logger/ViewController.swift
+++ ARKit-Data-Logger/ViewController.swift
@@ -9,16 +9,47 @@
 import UIKit
 import SceneKit
 import ARKit
+import os.log
+
 
 class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
+    // cellphone screen UI outlet objects
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet var sceneView: ARSCNView!
+    
+    
+    // constants for collecting data
+    var isRecording: Bool = false
+    let customQueue: DispatchQueue = DispatchQueue(label: "pyojinkim.me")
+    
+    
+    // variables for measuring time in iOS clock
+    var recordingTimer: Timer = Timer()
+    var secondCounter: Int64 = 0 {
+        didSet {
+            statusLabel.text = interfaceIntTime(second: secondCounter)
+        }
+    }
+    let mulSecondToNanoSecond: Double = 1000000000
+    
+    
+    // text file input & output
+    var fileHandler = [FileHandle]()
+    var fileURL = [URL]()
+    var fileName: String = "ARKit_data_collection.txt"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // set debug option
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        
+        // change status text to "Ready"
+        statusLabel.text = "Ready"
         
         
         // set the view's delegate
@@ -48,7 +79,61 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     }
     
     
-    // when the ARSession is updated
+    // when the Start/Stop button is pressed
+    @IBAction func startStopButtonPressed(_ sender: UIButton) {
+        if (self.isRecording == false) {
+            
+            // start ARKit data recording
+            customQueue.async {
+                if (self.createFiles()) {
+                    DispatchQueue.main.async {
+                        // reset timer
+                        self.secondCounter = 0
+                        self.recordingTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (Timer) -> Void in
+                            self.secondCounter += 1
+                        })
+                        
+                        // update UI
+                        self.startStopButton.setTitle("Stop", for: .normal)
+                        
+                        // make sure the screen won't lock
+                        UIApplication.shared.isIdleTimerDisabled = true
+                    }
+                    self.isRecording = true
+                } else {
+                    self.errorMsg(msg: "Failed to create the file")
+                    return
+                }
+            }
+        } else {
+            
+            // stop recording and share the recorded text file
+            if (recordingTimer.isValid) {
+                recordingTimer.invalidate()
+            }
+            
+            customQueue.async {
+                self.isRecording = false
+                for handler in self.fileHandler {
+                    handler.closeFile()
+                }
+                DispatchQueue.main.async {
+                    let activityVC = UIActivityViewController(activityItems: self.fileURL, applicationActivities: nil)
+                    self.present(activityVC, animated: true, completion: nil)
+                }
+            }
+            
+            // initialize UI on the screen
+            self.startStopButton.setTitle("Start", for: .normal)
+            self.statusLabel.text = "Ready"
+            
+            // resume screen lock
+            UIApplication.shared.isIdleTimerDisabled = false
+        }
+    }
+    
+    
+    // define if ARSession is didUpdate (callback function)
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         print("Frame updated!")
         // Do something with the new transform
@@ -58,5 +143,56 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         print("z: \(currentTransform.columns.3.z)")
         
         //print(currentTransform)
+    }
+    
+    
+    // some useful functions
+    private func errorMsg(msg: String) {
+        DispatchQueue.main.async {
+            let fileAlert = UIAlertController(title: "IMURecorder", message: msg, preferredStyle: .alert)
+            fileAlert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(fileAlert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    private func createFiles() -> Bool {
+        
+        // initialize file handlers
+        self.fileHandler.removeAll()
+        self.fileURL.removeAll()
+        
+        // create ARKit result text file
+        let header = "Created at \(timeToString())"
+        var url = URL(fileURLWithPath: NSTemporaryDirectory())
+        url.appendPathComponent(fileName)
+        self.fileURL.append(url)
+        
+        // delete previous text file
+        if (FileManager.default.fileExists(atPath: url.path)) {
+            do {
+                try FileManager.default.removeItem(at: url)
+            } catch {
+                os_log("cannot remove previous file", log:.default, type:.error)
+                return false
+            }
+        }
+        
+        // create new text file
+        if (!FileManager.default.createFile(atPath: url.path, contents: header.data(using: String.Encoding.utf8), attributes: nil)) {
+            self.errorMsg(msg: "cannot create file \(self.fileName)")
+            return false
+        }
+        
+        // assign new file handler
+        let fileHandle: FileHandle? = FileHandle(forWritingAtPath: url.path)
+        if let handle = fileHandle {
+            self.fileHandler.append(handle)
+        } else {
+            return false
+        }
+        
+        // return true if everything is alright
+        return true
     }
 }
