0ceb7725978a2391ecc2a510bdc8bf414bf2a753
pjinkim-PC\pjinkim <pjinkim1215@gmail.com>
2019-06-05 21:42:20 +0900

    add plot 3D point cloud part in Matlab visualization.

--- Visualization/main_script.m
+++ Visualization/main_script.m
@@ -5,36 +5,53 @@ rand('state',0); % rand('state',sum(100*clock));
 dbstop if error;
 
 
-%% plot ARKit VIO results
+%% 1) parse ARKit camera pose data
 
-% parsing ARKit data text file
-textFileDir = 'ARKit-data-collection.txt';
+% parsing ARKit camera pose data text file
+textFileDir = 'ARKit_camera_pose.txt';
 delimiter = ' ';
 headerlinesIn = 0;
-textARKitData = importdata(textFileDir, delimiter, headerlinesIn);
-ARKitTime = textARKitData(:,1).';
-ARKitTime = (ARKitTime - ARKitTime(1)) ./ 1000000000;
+textARKitPoseData = importdata(textFileDir, delimiter, headerlinesIn);
+ARKitPoseTime = textARKitPoseData(:,1).';
+ARKitPoseTime = (ARKitPoseTime - ARKitPoseTime(1)) ./ 1000000000;
 
 
-% ARKit camera pose with 4x4 transformation matrix (T_gc)
-ARKitPose = textARKitData(:,[2:13]);
+% ARKit camera pose with various 6-DoF camera pose representations
+ARKitPose = textARKitPoseData(:,[2:13]);
 numPose = size(ARKitPose,1);
 T_gc_ARKit = cell(1,numPose);
-for k = 1:numPose
-    T_gc_ARKit{k} = [reshape(ARKitPose(k,:).', 4, 3).'; [0, 0, 0, 1]];
-end
-
-
-% convert 6-DoF camera pose representation
 stateEsti_ARKit = zeros(6,numPose);
 R_gc_ARKit = zeros(3,3,numPose);
 for k = 1:numPose
+    
+    % rigid body transformation matrix (4x4)
+    T_gc_ARKit{k} = [reshape(ARKitPose(k,:).', 4, 3).'; [0, 0, 0, 1]];
+    
+    % state vector and rotation matrix
     R_gc_ARKit(:,:,k) = T_gc_ARKit{k}(1:3,1:3);
     stateEsti_ARKit(1:3,k) = T_gc_ARKit{k}(1:3,4);
     [yaw, pitch, roll] = dcm2angle(R_gc_ARKit(:,:,k));
     stateEsti_ARKit(4:6,k) = [roll; pitch; yaw];
 end
 
+
+%% 2) parse ARKit point cloud data
+
+% parsing ARKit point cloud data text file
+textFileDir = 'ARKit_point_cloud.txt';
+delimiter = ' ';
+headerlinesIn = 0;
+textARKitPointData = importdata(textFileDir, delimiter, headerlinesIn);
+ARKitPointTime = textARKitPointData(:,1).';
+ARKitPointTime = (ARKitPointTime - ARKitPointTime(1)) ./ 1000000000;
+
+
+% ARKit 3D point cloud
+ARKitPoints = textARKitPointData(:,[3:5]).';
+numPoints = size(ARKitPoints,2);
+
+
+%% plot ARKit VIO results
 
 % 1) play 3D trajectory of ARKit camera pose
 figure(10);
@@ -57,30 +74,34 @@ end
 % 2) plot ARKit VIO motion estimation results
 figure;
 h_ARKit = plot3(stateEsti_ARKit(1,:),stateEsti_ARKit(2,:),stateEsti_ARKit(3,:),'m','LineWidth',2); hold on; grid on;
+scatter3(ARKitPoints(1,:), ARKitPoints(2,:), ARKitPoints(3,:),'k.');
 plot_inertial_frame(0.5); legend(h_ARKit,{'ARKit'}); axis equal; view(26, 73);
 xlabel('x [m]','fontsize',10); ylabel('y [m]','fontsize',10); zlabel('z [m]','fontsize',10); hold off;
+
+% figure options
+f = FigureRotator(gca());
 
 
 % 3) plot roll/pitch/yaw of ARKit device orientation
 figure;
 subplot(3,1,1);
-plot(ARKitTime, stateEsti_ARKit(4,:), 'm'); hold on; grid on; axis tight;
+plot(ARKitPoseTime, stateEsti_ARKit(4,:), 'm'); hold on; grid on; axis tight;
 set(gcf,'color','w'); hold off;
-axis([min(ARKitTime) max(ARKitTime) min(stateEsti_ARKit(4,:)) max(stateEsti_ARKit(4,:))]);
+axis([min(ARKitPoseTime) max(ARKitPoseTime) min(stateEsti_ARKit(4,:)) max(stateEsti_ARKit(4,:))]);
 set(get(gcf,'CurrentAxes'),'FontName','Times New Roman','FontSize',17);
 xlabel('Time [sec]','FontName','Times New Roman','FontSize',17);
 ylabel('Roll [rad]','FontName','Times New Roman','FontSize',17);
 subplot(3,1,2);
-plot(ARKitTime, stateEsti_ARKit(5,:), 'm'); hold on; grid on; axis tight;
+plot(ARKitPoseTime, stateEsti_ARKit(5,:), 'm'); hold on; grid on; axis tight;
 set(gcf,'color','w'); hold off;
-axis([min(ARKitTime) max(ARKitTime) min(stateEsti_ARKit(5,:)) max(stateEsti_ARKit(5,:))]);
+axis([min(ARKitPoseTime) max(ARKitPoseTime) min(stateEsti_ARKit(5,:)) max(stateEsti_ARKit(5,:))]);
 set(get(gcf,'CurrentAxes'),'FontName','Times New Roman','FontSize',17);
 xlabel('Time [sec]','FontName','Times New Roman','FontSize',17);
 ylabel('Pitch [rad]','FontName','Times New Roman','FontSize',17);
 subplot(3,1,3);
-plot(ARKitTime, stateEsti_ARKit(6,:), 'm'); hold on; grid on; axis tight;
+plot(ARKitPoseTime, stateEsti_ARKit(6,:), 'm'); hold on; grid on; axis tight;
 set(gcf,'color','w'); hold off;
-axis([min(ARKitTime) max(ARKitTime) min(stateEsti_ARKit(6,:)) max(stateEsti_ARKit(6,:))]);
+axis([min(ARKitPoseTime) max(ARKitPoseTime) min(stateEsti_ARKit(6,:)) max(stateEsti_ARKit(6,:))]);
 set(get(gcf,'CurrentAxes'),'FontName','Times New Roman','FontSize',17);
 xlabel('Time [sec]','FontName','Times New Roman','FontSize',17);
 ylabel('Yaw [rad]','FontName','Times New Roman','FontSize',17);
