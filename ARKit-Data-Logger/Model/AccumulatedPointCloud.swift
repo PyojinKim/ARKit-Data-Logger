f3361e33e67dffe271e6fbb61ec4a83d732645e8
pjinkim-Mac-PC <pjinkim1215@gmail.com>
2019-06-27 18:00:27 +0900

    update app icon and MVC structure.

---
+++ ARKit-Data-Logger/Model/AccumulatedPointCloud.swift
@@ -0,0 +1,18 @@
+//
+//  AccumulatedPointCloud.swift
+//  ARKit-Data-Logger
+//
+//  Created by kimpyojin on 27/06/2019.
+//  Copyright © 2019 Pyojin Kim. All rights reserved.
+//
+
+import Foundation
+
+class AccumulatedPointCloud {
+    
+    
+    
+    
+    
+    
+}
